03f5bd869d6413b79f6343589080c40a6960b8fe
morwen80 <ribesnero80@gmail.com>
2019-09-02 18:32:13 +0100

    make a single HTTP request instead of splitting it into 2

--- scraper.js
+++ scraper.js
@@ -6,25 +6,21 @@ const url = `https://www.waterstones.com/books/search/term/`
 const searchBooks = (title) => {
     return fetch(`${url}${title}/format/16`)
         .then(resp => resp.text())
-}
-
-searchBooks(`Harry Potter`)
-    .then(body => {
-
-        const books = []
-
-        const $ = cheerio.load(body);
-        $('.inner').each(function(index, element) {
-            const $element = $(element);
-            const $image = $element.find('.image-wrap a img');
-            const $title = $element.find('.title-wrap a');
-            const $author = $element.find('.author a')
-            const book = {
-                cover: $image.attr('src'),
-                title: $title.text(),
-                author: $author.text()
-            };
-            books.push(book);
+        .then(body => {
+            const books = []
+            const $ = cheerio.load(body);
+            $('.inner').each(function(index, element) {
+                const $element = $(element);
+                const $image = $element.find('.image-wrap a img');
+                const $title = $element.find('.title-wrap a');
+                const $author = $element.find('.author a')
+                const book = {
+                    cover: $image.attr('src'),
+                    title: $title.text(),
+                    author: $author.text()
+                };
+                books.push(book);
+            })
+            return books
         })
-        console.log(books)
-    })
+}   
