9e01a25d38e2aff8232f56badc9cd55789fa2fac
morwen80 <ribesnero80@gmail.com>
2019-09-02 22:35:32 +0100

    add page url to the scraped JSON object

--- scraper.js
+++ scraper.js
@@ -11,13 +11,17 @@ const searchBooks = (title) => {
             const $ = cheerio.load(body);
             $('.inner').each(function(index, element) {
                 const $element = $(element);
-                const $image = $element.find('.book-thumb .image-wrap a img');
+                const $image = $element.find('.image-wrap a img');
                 const $title = $element.find('.title-wrap a');
-                const $author = $element.find('.author a')
+                const $author = $element.find('.author a');
+                
+                const $bookPage = $element.find('.image-wrap a');
+                const $bookUrl = `https://www.waterstones.com`;
                 const book = {
                     cover: $image.attr('src'),
                     title: $title.text(),
-                    author: $author.text()
+                    author: $author.text(),
+                    pageUrl: $bookUrl + $bookPage.attr('href')
                 };
                 if(book.cover) {
                     books.push(book)
